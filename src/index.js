5f98125268ba86289bd51e57a9df6ce86fc6c93d
tra tan sang <sang.tra@dirox.net>
2018-09-13 10:11:26 +0700

    codepush

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import codePush from 'react-native-code-push';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+        <Text>langtutasnagn</Text>
+    );
+  }
+}
+
+export default codePush(App);
