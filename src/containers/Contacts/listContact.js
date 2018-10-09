df41403094c85b110ce51bbf9321af07800f078f
tra tan sang <sang.tra@dirox.net>
2018-10-09 10:55:50 +0700

    done

--- src/containers/Contacts/listContact.js
+++ src/containers/Contacts/listContact.js
@@ -26,12 +26,18 @@ class ListContact extends Component {
   setContact = (dataContacts) => {
 
     let arrayChar = [...Array(26)].map((_, i) => (String.fromCharCode('A'.charCodeAt(0) + i)));
-    let data = {};
+
+    let data =  [...Array(26)].map((_, i) => (
+      {
+        title:String.fromCharCode('A'.charCodeAt(0) + i), items : []
+      } 
+    ));
+    data.push({title: "#", items: []})
     dataContacts = dataContacts.forEach(e => {
       let givenName = [e.givenName, e.middleName, e.familyName].filter(e => !!e).join(' ');
-      let index = givenName.indexOf(arrayChar) !== -1 ? arrayChar[index] : '#';
-      if(!data[index]) data[index] = {item:[]};
-      data[index].item.push( {
+      let index = arrayChar.indexOf(givenName.charAt(0).toUpperCase())
+      if( index === - 1) index = 26;
+      data[index].items.push( {
         givenName,
         id: e.recordID,
         thumbnailPath: e.thumbnailPath,
@@ -39,9 +45,8 @@ class ListContact extends Component {
       })
       
     })
-    data = Object.keys(data).map(function(e,key) {
-      return { title: key, item : e.item}
-    });
+    data = data.filter(e => e.items.length > 0)
+    console.log(data);
     this.setState({ dataContacts: data })
   }
 
@@ -94,47 +99,42 @@ class ListContact extends Component {
     this.setState({ key });
   }
   
-  renderItem = (section) => {
+  renderSectionItem = (section) => {
     let {dataContacts} = this.state
     let item = dataContacts[section]
-    return (!!item.divider ?
-      <ListItem itemDivider >
-        <Text>{item.givenName}</Text>
-      </ListItem> :
-      <ListItem
-        button onPress={() => this.onPress(item.id)}
-        noIndent
-        style={{ backgroundColor: item.id === this.state.key ? "#cde1f9" : '#fff' }}>
-        <Left style={{ flex: 1 }}>
-          <Thumbnail square source={!!item.thumbnailPath ? { uri: item.thumbnailPath } : require('@thumbnails/category/default-contact.png')} />
-        </Left>
-        <Body style={{ flex: 4 }}>
-          <Text>{item.givenName}</Text>
-          {item.phoneNumbers.length > 0 && item.phoneNumbers.map((ele, keyele) => <Text key={keyele} note>{ele.number}</Text>)}
-        </Body>
-      </ListItem>
-
+    return (
+      <ListItem itemDivider>
+        <Text>{item.title}</Text>
+      </ListItem> 
     )
+  }
+  renderIndexItem = ({section, row}) => {
+    let {dataContacts} = this.state
+    let item = dataContacts[section].items[row]
+    return(
+    <ListItem
+    button onPress={() => this.onPress(item.id)}
+    noIndent
+    style={{ backgroundColor: item.id === this.state.key ? "#cde1f9" : '#fff' }}>
+    <Left style={{ flex: 1 }}>
+      <Thumbnail square source={!!item.thumbnailPath ? { uri: item.thumbnailPath } : require('@thumbnails/category/default-contact.png')} />
+    </Left>
+    <Body style={{ flex: 4 }}>
+      <Text>{item.givenName}</Text>
+      {item.phoneNumbers.length > 0 && item.phoneNumbers.map((ele, keyele) => <Text key={keyele} note>{ele.number}</Text>)}
+    </Body>
+  </ListItem>)
   }
   render() {
     let { dataContacts } = this.state;
     return (
-        // <FlatList 
-        //   data={dataContacts}
-        //   extraData={this.state}
-        //   initialNumToRender={8}
-        //   maxToRenderPerBatch={2}
-        //   onEndReachedThreshold={0.5}
-        //   renderItem={this.renderItem}
-        //   keyExtractor={ ({item,index}) => index+'key' }
-
-        // />
+        dataContacts.length === 0 ? <Text>Đang lấy dữ liệu</Text> :
         <LargeList
           data={dataContacts}
           heightForSection={() => 50}
-          renderSection={this.renderItem}
-          heightForIndexPath={() => 50}
-          renderIndexPath={this.renderItem}
+          renderSection={this.renderSectionItem}
+          heightForIndexPath={() => 80}
+          renderIndexPath={this.renderIndexItem}
         />
 
     )
